f8139aac0f10ad772d6c1e453c4b3b493a708ffd
Rain_G <77776581+Rain156@users.noreply.github.com>
2024-08-03 22:07:07 +0800

    Delete zh_cn directory

--- zh_cn/kubejs/startup_scripts/main.js
+++
@@ -1,7 +0,0 @@
-// priority: 0
-
-StartupEvents.registry('mob_effect', event => {
-    event.create('tired').modifyAttribute('minecraft:generic.movement_speed',
-    '8b50fa8d-9f78-47a7-93c6-9bbeb36587f9',
-    -2, 'addition').color(Color.GRAY).displayName('Tired')
-})
